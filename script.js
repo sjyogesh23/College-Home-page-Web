203b7ea15f1a63dade94d285b8b5564e82182a89
Yogesh <sjyogesh23@gmail.com>
2023-01-01 11:02:53 +0530

    Update

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-let valueDisplays = document.querySelectorAll(".num");
-let interval = 1000;
-
-valueDisplays.forEach((valueDisplay) => {
-    let startvalue = 0;
-    let endvalue = parseInt(valueDisplay.getAttribute("data-value"));
-    let duration = Math.floor(interval / endvalue);
-    let counter = setInterval(function(){
-        startvalue += 1;
-        valueDisplay.textContent = startvalue;
-        if(startvalue == endvalue){
-            clearInterval(counter);
-        }
-    }, duration);
-});
-
+function scrol(){
+    let valueDisplays = document.querySelectorAll(".num");
+    let interval = 1000;
+    var srl = window.scrollY;
+        valueDisplays.forEach((valueDisplay) => {
+            
+            
+            let startvalue = 0;
+            let endvalue = parseInt(valueDisplay.getAttribute("data-value"));
+            let duration = Math.floor(interval / endvalue);
+            if(srl>240){
+                let counter = setInterval(function(){
+                    startvalue += 1;
+                    valueDisplay.textContent = startvalue;
+                    if(startvalue == endvalue){
+                        clearInterval(counter);
+                    }
+                }, duration);
+            }
+        });
+}
+window.addEventListener('scroll',scrol);
